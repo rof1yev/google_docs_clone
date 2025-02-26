7027c4f5adfdfdd490421d61d44bd727d33eb13c
rof1yev <rofiyevdilshod@gmail.com>
2025-02-26 23:12:46 +0500

    tiptap extentions

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,15 @@
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Heading from "@tiptap/extension-heading";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
 
 export const Editor = () => {
   const editor = useEditor({
@@ -12,8 +21,39 @@ export const Editor = () => {
           "focus:outline-none print:border-none bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      StarterKit,
+      Image,
+      ImageResize,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Heading.configure({
+        levels: [1, 2, 3],
+      }),
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+    ],
+    content: `
+		<p>Hello World! 🌎️</p>
+		<table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+		`,
   });
 
   return (
