63354f7b6984b4dad228c9010c68c37ffef65ed3
rof1yev <rofiyevdilshod@gmail.com>
2025-03-08 19:22:39 +0500

    navigation bar component

---
+++ src/app/documents/[documentId]/document-input.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { BsCloudCheck } from "react-icons/bs";
+
+export const DocumentInput = () => {
+  return (
+    <div className="flex items-center gap-2">
+      <span className="text-lg px-1.5 cursor-pointer truncate">
+        Untitled Document
+      </span>
+      <BsCloudCheck className="" />
+    </div>
+  );
+};
