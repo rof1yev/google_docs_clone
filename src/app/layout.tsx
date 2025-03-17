0bb3747b6c528eca5ef5fdcd57fd4208571f53e8
rof1yev <rofiyevdilshod@gmail.com>
2025-03-17 21:57:37 +0500

    Authentication setup (Clerk)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
-import "./globals.css";
 import { ConvexClientProvider } from "@/providers/convex-client-provider";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/providers/convex-client-provider.tsx
+++ src/providers/convex-client-provider.tsx
@@ -1,10 +1,39 @@
 "use client";
 
 import { ReactNode } from "react";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import {
+  ConvexReactClient,
+  AuthLoading,
+  Authenticated,
+  Unauthenticated,
+} from "convex/react";
+import {
+  ClerkProvider,
+  SignInButton,
+  useAuth,
+  SignIn,
+} from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { FullScreenLoader } from "@/components/fullscreen-loader";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <Authenticated>{children}</Authenticated>
+        <Unauthenticated>
+          <div className="min-h-screen flex flex-col justify-center items-center">
+            <SignIn />
+          </div>
+        </Unauthenticated>
+        <AuthLoading>
+          <FullScreenLoader label="Authentication loading..." />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
